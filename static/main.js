a8e8867f9eb6a1cbff57eb35257c587dfc7bd894
German Cabarcas <gcc@unironical.com>
2021-03-06 01:20:25 -0500

    fixed aspect ratio of slider

--- static/main.js
+++ static/main.js
@@ -1,36 +1,36 @@
 window.addEventListener("DOMContentLoaded", function () {
-const caroussel = document.querySelectorAll(".hr-Slider");
-if (caroussel.length) {
-  console.log("1got hit");
-const sliderControls = document.querySelectorAll(".sl-Slider_Control");
+  const caroussel = document.querySelectorAll(".hr-Slider");
+  const sliderWidth = `${document.querySelector(".hr-SliderWrapper").getBoundingClientRect().width}px`;
+  document.documentElement.style.setProperty("--slider-width", sliderWidth)
+  const sliderHeight = `${document.querySelector(".hr-SliderWrapper").getBoundingClientRect().height}px`;
+  document.documentElement.style.setProperty("--slider-height", sliderHeight)
+  if (caroussel.length) {
+    console.log(sliderHeight, sliderWidth);
+  const sliderControls = document.querySelectorAll(".sl-Slider_Control");
 
-caroussel.forEach((card) => {
-  console.log("got hit");
-  console.log( card );
-  const imagesContainer = card.querySelector(".hr-Slider");
-  console.log( imagesContainer );
-  sliderControls.forEach((el, index) => {
-    el.addEventListener(`click`, (e) => {
-      toggleSlider(e);
-      document.querySelector('.sl-Slider_Control[aria-current="true"]').setAttribute('aria-current', false) 
-      e.target.setAttribute('aria-current', true);
-      if (e.isTrusted) clearInterval( sliderTicker );
+  caroussel.forEach(function(card) {
+    sliderControls.forEach((el, index) => {
+      el.addEventListener(`click`, (e) => {
+        toggleSlider(e);
+        document.querySelector('.sl-Slider_Control[aria-current="true"]').setAttribute('aria-current', false) 
+        e.target.setAttribute('aria-current', true);
+        if (e.isTrusted) clearInterval( sliderTicker );
+      });
     });
+    function toggleSlider (event) {
+      console.log("togglin");
+      event.preventDefault();
+      const index = event.target.dataset.index;
+      const displacementBase = document.querySelector(`.sl-slide`).getBoundingClientRect().width;
+      card.style.transform = `translate3d(-${index * displacementBase}px, 0 ,0)`;
+    }
   });
-  function toggleSlider (event) {
-    console.log("togglin");
-    event.preventDefault();
-    const index = event.target.dataset.index;
-    const displacementBase = document.querySelector(`.sl-slide`).getBoundingClientRect().width;
-    card.style.transform = `translate3d(-${index * displacementBase}px, 0 ,0)`;
+
+  let sliderTicker = window.setInterval(function() {
+    if (!!document.querySelector('[aria-current="true"]').nextElementSibling) document.querySelector('[aria-current="true"]').nextElementSibling.click()
+    else { sliderControls[0].click() }
+  }, 5000)
+
+  // sliderTicker;
   }
-});
-
-let sliderTicker = window.setInterval(function() {
-  if (!!document.querySelector('[aria-current="true"]').nextElementSibling) document.querySelector('[aria-current="true"]').nextElementSibling.click()
-  else { sliderControls[0].click() }
-}, 5000)
-
-sliderTicker;
-}
 })
